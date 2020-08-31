22044cd2cd99c7b55177b4eb8cc57a646b20d227
mukesh kumar <mk-56@outlook.com>
2020-08-31 13:27:37 +0530

    Add commit

--- Section 17/ColorGame 2/ColorGame2.js
+++ Section 17/ColorGame 2/ColorGame2.js
@@ -1,4 +1,4 @@
-var color =[
+var colors =[
   "rgb(255,0,0)",
   "rgb(255,255,0)",
   "rgb(0,255,0)",
@@ -7,14 +7,14 @@ var color =[
   "rgb(255,0,255)",
 ]
 var squares = document.querySelectorAll(".square");
-var pickedColor = color[3];
+var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i < squares.length; i++){
 
-  squares[i].style.background = color[i];
+  squares[i].style.background = colors[i];
   
    squares[i].addEventListener("click", function(){
       var clickedColor = this.style.background;
