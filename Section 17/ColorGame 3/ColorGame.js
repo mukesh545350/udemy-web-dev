97f8dfceb296127cbaae445128927691bd27ea20
mukesh <mk-56@outlook.com>
2020-09-01 08:10:47 +0530

    Add commit

---
+++ Section 17/ColorGame 3/ColorGame.js
@@ -0,0 +1,29 @@
+var color =[
+  "rgb(255,0,0)",
+  "rgb(255,255,0)",
+  "rgb(0,255,0)",
+  "rgb(0,255,255)",
+  "rgb(0,0,255)",
+  "rgb(255,0,255)",
+]
+var squares = document.querySelectorAll(".square");
+var pickedColor = color[3];
+var colorDisplay = document.getElementById("colorDisplay");
+
+colorDisplay.textContent = pickedColor;
+
+for(var i = 0; i < squares.length; i++){
+
+  squares[i].style.background = color[i];
+  
+    squares[i].addEventListener("click", function(){
+    
+    var clickedColor = this.style.background;
+    
+    if(clickedColor === pickedColor){
+      alert("Correct!");
+    } else {
+      this.style.background = "#232323";
+    }
+  });
+}
