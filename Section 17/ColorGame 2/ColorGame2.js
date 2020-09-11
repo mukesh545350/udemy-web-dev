7ad1f20a7dc7bbaf516fa5aad19c8175ee28405b
mukesh <mk-56@outlook.com>
2020-09-11 12:48:06 +0530

    Add

--- Section 17/ColorGame 2/ColorGame2.js
+++ Section 17/ColorGame 2/ColorGame2.js
@@ -1,10 +1,10 @@
 var colors = [
-  "rgb(255,0,0)",
-  "rgb(255,255,0)",
-  "rgb(0,255,0)",
-  "rgb(0,255,255)",
-  "rgb(0,0,255)",
-  "rgb(255,0,255)"
+  "rgb(255, 0, 0)",
+  "rgb(255, 255, 0)",
+  "rgb(0, 255, 0)",
+  "rgb(0, 255, 255)",
+  "rgb(0, 0, 255)",
+  "rgb(255, 0, 255)"
 ]
 var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
